70011a347b0ef13852c99f0fb6f622f79c61a942
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-04-03 11:48:37 +0900

    Made the Stock Picker even shinier

--- Stock_Picker.rb
+++ Stock_Picker.rb
@@ -1,9 +1,11 @@
 # stocks must be an array with integers only
 # Returns an array with 3 integers: [Max Profit, Buy on day, Sell on day]
-# If no profit can be made, it will return the following array: [0, 0, 0]
+# If no profit can be made, result[0] == 0
 def stock_picker(stocks)
 	# Until the array stocks' size is 2 or less it will call the stock_picker method recursively for stocks[1..last]
 	max_profit_on_days = stocks.length > 2 ? stock_picker(stocks[1..(stocks.length - 1)]) : [0, 0, 0]
+	max_profit_on_days[1] += 1
+	max_profit_on_days[2] += 1
 	# Calculate the best sell day and the max profit if bought on day 1
 	buy_day = 0
 	for sell_day in 1..(stocks.length - 1)
@@ -18,6 +20,18 @@ def stock_picker(stocks)
 	return max_profit_on_days
 end
 
-# Run the code for a given array
-hindsight = stock_picker([17,3,6,9,15,8,6,1,10])
-puts "You would've made a maximum profit of #{hindsight[0]}$,\nif only you had bought on Day #{hindsight[1]} and sold on Day #{hindsight[2]}..."
+# Run the code for a given array of integers
+def run(stocks)
+	puts "List of stock prices:"
+	i = 1
+	stocks.each do |stock|
+		puts "Day #{i}: #{stocks[i - 1]}$"
+		i += 1
+	end
+	hindsight = stock_picker(stocks)
+	puts "You would've made a maximum profit of #{hindsight[0]}$,\nif only you had bought on Day #{hindsight[1]} and sold on Day #{hindsight[2]}..."
+end
+
+# Run the code
+stocks = [17,3,6,9,15,8,6,1,10]  # Must be an array of integers only
+run(stocks)
